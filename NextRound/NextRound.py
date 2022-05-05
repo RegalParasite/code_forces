c1775b87307e8da3fb6ae7200eb6e13148cc0de7
RegalParasite <jedamove@hotmail.com>
2022-05-05 08:46:45 -0500

    NextRound 1.1

---
+++ NextRound/NextRound.py
@@ -0,0 +1,11 @@
+places = str(input()).split(' ')
+score = str(input()).split(' ')
+
+score_places = int(score[int(places[-1])])
+total = 0
+
+for i in score:
+    if i != '0' and int(i) >= score_places:
+
+        total += 1
+print(total)
